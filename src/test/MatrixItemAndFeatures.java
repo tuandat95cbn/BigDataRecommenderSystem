60ee32d9b3e65ed1009dbfd4fdcb475a8fd2dbb4
Dang Quang Trung <20134145@student.hust.edu.vn>
2017-01-07 03:36:33 +0700

    update code

--- src/test/MatrixItemAndFeatures.java
+++ src/test/MatrixItemAndFeatures.java
@@ -336,11 +336,11 @@ public class MatrixItemAndFeatures {
 		System.out.println(listFeature);
 		System.out.println(listFilmOfFeature);*/
 		MatrixItemAndFeatures matrix = new MatrixItemAndFeatures();
-		matrix.readFileMovie("ml-latest-small/movies.csv");
+		matrix.readFileMovie("ml-20m/movies.csv");
 		matrix.writeFeatureOfMovie("Data/Features.txt");
 		matrix.writeMovie("Data/Movie_Features.txt");
 		matrix.writeMoiveAndId("Data/Movies_Title.txt");
-		//matrix.initMatrixMovieFeature();
+		matrix.initMatrixMovieFeature();
 		matrix.writeMatrix("Data/Matrix_Content.txt");
 		System.out.println("DONE!");
 	}
